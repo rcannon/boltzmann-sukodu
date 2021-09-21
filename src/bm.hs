49c04b02fae4f0098fa66fa7285c1d2753e0d532
rcannon <reillycannon@gmail.com>
2021-09-20 17:23:31 -0700

    import all as qualified

--- src/bm.hs
+++ src/bm.hs
@@ -3,16 +3,19 @@ module Bm where
 
 import qualified Data.Vector.Generic as V
 import qualified Numeric.LinearAlgebra as LA
-import Numeric.LinearAlgebra.Data
+import qualified Numeric.LinearAlgebra.Data as LAD
 
 type Excludes = [Bool]
 type Index = Int
 type Value = Double
-
 type Temperature = Value
+type Vector = LAD.Vector
+type Matrix = LAD.Matrix
+
 type Values = Vector Value
 type Biases = Vector Value
 type Weights = Matrix Value
+
 data BM = BM Weights Biases Values
 
 scaleValForUpdate :: Temperature -> Value -> Value
@@ -20,5 +23,5 @@ scaleValForUpdate t = negate . (/ t)
 
 getValForUpdate :: BM -> Index -> Value
 getValForUpdate (BM wss bs vs) idx 
-  = flip (!) idx $ LA.add bs $ (LA.#>) wss $ (V.//) vs [(idx, 0.0)]
+  = flip (LAD.!) idx $ LA.add bs $ (LA.#>) wss $ (V.//) vs [(idx, 0.0)]
       
