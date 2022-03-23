de24da66063513940af819b63d1396ce8c8d0fac
rcannon <reillycannon@gmail.com>
2022-03-23 08:18:57 -0500

    clean up

---
+++ src/BMBase2.hs
@@ -0,0 +1,13 @@
+
+module BMBase where
+
+--import qualified Data.Vector.Unboxed as V
+import qualified Data.Array.Repa as R
+
+type VectorType = R.Z R.:. Int
+type MatrixDim = R.Z R.:. Int R.:. Int
+
+
+-- get value at index
+
+
